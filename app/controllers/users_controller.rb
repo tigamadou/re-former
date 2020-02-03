df6efe2ea5ec37e4fa98dfe6885122a45af8fc9f
Amadou Ibrahim <amadou140589@gmail.com>
2020-02-03 23:15:04 +0100

    User new view created and method filled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
-    def new
-    end
-    
-    def create
-    end
+	def new
+	end
+	
+	def create
+		@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+		if @user.save
+				redirect_to new_user_path, flash: {success: 'User created!'}
+		else
+			redirect_to new_user_path, flash: {danger: 'Oops. This email is already used!'}
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     validates :username, :password, :email, presence: true
+    validates :email, presence: true, uniqueness:true
 end
